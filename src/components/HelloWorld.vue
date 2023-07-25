ab9611d56acd80cc35cb57eafa341b9c0f037200
Pranay Prasad <pranayprasad42@gmail.com>
2023-07-25 17:22:08 +0530

    Chartjs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,77 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <ChartData tableData :="tableData" /> -->
+    <!-- {{ tableData  }} -->
+    <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
   </div>
 </template>
 
 <script>
-export default {
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default ({
   name: 'HelloWorld',
+  components: {
+    Bar
+  },
   props: {
     msg: String
+  },
+  data() {
+    return {
+      tableData: [],
+      countperMonth: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      chartData: {
+        datasets: [{ data: [1,2,3,0,0,0,5,6,7,15,9,1] }],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  },
+  methods: {
+    async getChart() {
+      for (let index = 0; index < this.tableData.length; index++) {
+        const element = this.tableData[index];
+        this.countperMonth[parseInt(element.order_date.substr(5,2))]++;
+      }
+      this.chartData.datasets[0].data = this.countperMonth;
+      console.log(this.chartData.datasets);
+    },
+    async fetchData() {
+      console.log("RUN");
+      await fetch("https://bee.nutshell.repair/api/v1/emp/reports/all_data?city=1&from_date=2023-01-01&to_date=2023-07-17&category[]=1&category[]=2&category[]=3&category[]=4&category[]=5&category[]=6&category[]=7&category[]=8&category[]=9", {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBfaWQiOjEwLCJlbXBfbW9iaWxlIjoiOTM3MzQ0ODAxNiIsInRva2VuX2NyZWF0ZWRfYXQiOjE2ODk1Nzk1OTMsInRva2VuX2V4cGlyZXNfYXQiOjE2OTEzMDc1OTN9.nW6EstOefPuhe7bD-mpIt2x4tzoga-RU5hXeUcThRis'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          this.tableData = data.data;
+          console.log(this.tableData);
+          this.getChart();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+    }
+
+  },
+  async created() {
+    await this.fetchData();
+  },
+  mounted(){
+    this.fetchData(); 
   }
-}
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -44,14 +79,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
---
+++ src/components/LineChart.vue
@@ -0,0 +1,26 @@
+<script>
+import { Line } from 'vue-chartjs';
+export default ({
+    extends: Line,
+    props:{
+        // label:{type:String},
+        chartData:{
+            type: Array
+        },
+        options:{type:Object}
+    },
+    mounted(){
+        const Maindata = this.chartData;
+        this.renderChart({
+            labels:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets:[
+                {
+                    data:Maindata
+                }
+            ]
+        },
+        this.options
+        )
+    }
+})
+</script>
